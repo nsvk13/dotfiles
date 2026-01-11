417e05e45dc88b30dcba3f3e3773198ffa50da3e
nsvk13 <nsvkdev@gmail.com>
2026-01-11 11:54:56 +0300

    fix home-manager

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -16,8 +16,8 @@
         system = "x86_64-linux";
         modules = [
           ./hosts/laptop/configuration.nix
-          
-          # Home Manager
+
+          home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
